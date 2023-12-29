316162c9bd02a275769d9e5b2768465cb76faf04
WilbertFE <wilbertbernadi699@gmail.com>
2023-12-29 13:58:09 +0700

    Mengerjakan navbar #2

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+
+// toggle Button
+const toggle = document.querySelector('#toggle');
+toggle.addEventListener('click', function(){
+    toggle.classList.toggle('button-active');
+});
+
+// scroll navbar
+const navBar = document.querySelector('header');
+window.onscroll = function (){
+    if (window.scrollY > navBar.offsetTop){
+        navBar.classList.add('navbar-fixed');
+    }
+};
