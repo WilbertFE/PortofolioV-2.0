baa7776043b570ae17e7e4cb333c3a056ac4153a
WilbertFE <wilbertbernadi699@gmail.com>
2024-01-02 08:16:08 +0700

    Mengatur ulang section portofolio dan beberapa img

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,11 @@ module.exports = {
         primary: '#0ea5e9',
         secondary: '#4ade80',
         abu: '#f1f5f9',
-        dark: '#0f172a'
+        dark: '#0f172a',
+        html: '#E34F26',
+        css: '#1572B6',
+        js: '#F7DF1E',
+        tw: '#06B6D4',
       }
     },
   },
