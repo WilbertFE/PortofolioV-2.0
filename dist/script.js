25654c0d121b94cd5d351bb4705d1f539d1ffec0
WilbertFE <wilbertbernadi699@gmail.com>
2024-01-02 09:07:28 +0700

    navigasi about selesai

--- dist/script.js
+++ dist/script.js
@@ -1,16 +1,24 @@
 
 // toggle Button
-const toggle = document.querySelector('#toggle');
 toggle.addEventListener('click', function(){
+    const toggle = document.querySelector('#toggle');
     toggle.classList.toggle('button-active');
 });
 
 // scroll navbar
-const navBar = document.querySelector('header');
 window.onscroll = function (){
+    const navBar = document.querySelector('header');
     if (window.scrollY > navBar.offsetTop){
         navBar.classList.add('navbar-fixed');
     } else {
         navBar.classList.remove('navbar-fixed');
     }
 };
+
+// Klik di mana aja
+window.onclick = function (e){
+    const toggle = document.querySelector('#toggle');
+    if(e.target != toggle){
+        toggle.classList.remove('button-active');
+    }
+}
